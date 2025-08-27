418d147b7180163a506b694f3e77efba7962d70e
expertproducer <expertproducer121@gmail.com>
2025-08-27 20:01:46 +0600

    1

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Building Next.js project...
-npm run build
-echo Copying out folder to dist...
-if exist dist rmdir /s /q dist
-xcopy out dist /e /i /h /y
-echo Build complete! Files are in dist folder.

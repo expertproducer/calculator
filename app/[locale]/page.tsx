6a6e15a4e3a59b7c0a8b6f8b9a25a1bb215ee5ee
expertproducer <expertproducer121@gmail.com>
2025-08-27 23:26:50 +0600

    1

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,8 @@
-import { getContent } from '../lib/i18n'
-import Hero from '../components/Hero'
-import { Problem, Services, Process, Deliverables, Benefits, Cases, Pricing, FAQ, Contact, Footer, Navbar } from '../components/AllComponents'
-import StructuredData from '../components/StructuredData'
-import CookieConsent from '../components/CookieConsent'
+import { getContent } from '../../lib/i18n'
+import Hero from '../../components/Hero'
+import { Problem, Services, Process, Deliverables, Benefits, Cases, Pricing, FAQ, Contact, Footer, Navbar } from '../../components/AllComponents'
+import StructuredData from '../../components/StructuredData'
+import CookieConsent from '../../components/CookieConsent'
 
 export async function generateStaticParams() {
   return [
